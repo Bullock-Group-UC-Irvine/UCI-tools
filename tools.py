0df981067876e51aeac98f4ed1265acc2333d9e2
Olti Myrtaj <oltimyrtaj@dhcp-v055-095.mobile.uci.edu>
2023-06-05 13:54:38 -0700

    fe_over_h function added by olti

--- tools.py
+++ tools.py
@@ -17,3 +17,15 @@ def read_snapshot_simple( filepath, particle_type='PartType0' ):
     p = pd.DataFrame( data )
             
     return p
+
+def fe_over_h_ratios(mfrac,he_frac,fe_frac):
+    h_frac=1-mfrac-he_frac
+    #...some constants                                                                                           
+    sun_fe_h_frac = 0.0030/91.2
+    mass_h = 1.0084 # in Atomic Mass Units                                                                                              
+    mass_fe= 55.845 # in Atomic Mass Units
+    #...Need to convert mass fractions to number fractions                                                                                                
+    fe_h_num = (fe_frac/h_frac)*(mass_h/mass_fe)
+    #...Abundance ratio                                                                                                                            
+    ab_fe_h = np.asarray(np.log10(fe_h_num/sun_fe_h_frac))
+    return ab_fe_h
