689e7e96da90381230f6bf9d3a67fb79dd876f1d
Patrick <patrickstaudt1@gmail.com>
2023-09-13 13:34:37 -0700

    added some doc strings to tools.py

--- tools.py
+++ tools.py
@@ -1,10 +1,45 @@
 import h5py
 import pandas as pd
 
-def save_var_latex(key, value):
+try:
+    '''
+    The user should have a paths.py script defining the idiosyncratic
+    locations of folders which the methods below need to use.
+    E.g. The contents of the user's paths.py might be as follows:
+        data = '$HOME/project/data/'
+        paper = '$HOME/project/paper' 
+    This is necessary because each user and each project will have a
+    different file structure
+    '''
+    import paths
+except:
+    pass
+
+def save_var_latex(key, value, fname='data.txt'):
+    '''
+    Save a data point do a file in paths.paper so the user's LaTeX paper can
+    use it.
+
+    Parameters
+    ----------
+    key: str
+        The key that the LaTeX paper will use to look up this data point.
+    value: object
+        The value of the data point the user wants to save for the LaTeX paper
+        to use. The code is written so any object (e.g. float, int, str) should
+        work, but it's probably best to use a str.
+    fname: str, default 'data.txt'
+        The file in paths.paper to which the code should save the prediction 
+        and its uncertainty.
+
+    Returns
+    -------
+    None
+    '''
+
     dict_var = {}
 
-    file_path = paths.paper +  "data.txt"
+    file_path = paths.paper + fname 
 
     try:
         with open(file_path, newline="") as file:
@@ -14,6 +49,10 @@ def save_var_latex(key, value):
     except FileNotFoundError:
         pass
 
+    # The code I took this from was written to handle saving multiple datum at
+    # once, so that's why it's written this way even though we only have one
+    # key, value pair. I'm keeping the code as in in case we want to expand its
+    # functionality in the future.
     dict_var[key] = value
 
     with open(file_path, "w") as f:
@@ -22,19 +61,30 @@ def save_var_latex(key, value):
 
     return None
 
-def save_prediction(string, y, dy):
+def save_prediction(string, y, dy, fname='data.txt'):
     '''
-    Save strings to data.txt to be used by paper.tex.
+    Save a prediction and its uncertainty to `fname` to be used by a Latex 
+    file.
 
     Parameters
     ----------
     string: str 
 	The identifier for the variable
+
     y: str 
         The value of the target variable
+
     dy: np.ndarray of str
 	The uncertainty in the variable. The user *must* provide it as an
         np.ndarray, even if it's only one single symmetric uncertainty.
+
+        E.g. If the error is symmetric, the user would provide something like
+             `dy=np.array(['0.1'])`.
+             If asymmetric, `dy=np.array(['0.1', '0.2'])`.
+
+    fname: str, default 'data.txt'
+        The file in paths.paper to which the code should save the prediction 
+        and its uncertainty.
 
     Returns
     -------
@@ -53,7 +103,6 @@ def save_prediction(string, y, dy):
     else:
         raise ValueError('Margin of error has a maximum of two elements.')
     return None
-
 
 def read_snapshot_simple( filepath, particle_type='PartType0' ):
     
