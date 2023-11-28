e95dc3946cff49ad395dc18bc27b3221954ed065
patricks1 <patrickstaudt1@gmail.com>
2023-11-27 17:36:43 -0800

    Made setup.py so that pip installs an executable console script UCI-tools.refactor that the user can use anywhere in the terminal. Also added a function `get_data` to tools.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="UCI_tools",
-    version="0.2.2",
+    version="0.3.0-beta",
     author="UCI Bullock Galaxy Dark Matter Group",
     author_email="bullock@uci.edu",
     description="Software tools developed by and commonly used by the group.",
@@ -27,4 +27,9 @@ setuptools.setup(
         'mock',
         'h5py',
     ],
+    entry_points={
+        'console_scripts': [
+            'UCI-tools.refactor = UCI_tools.refactor:main',
+        ],
+    }
 )
