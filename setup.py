81096a6743155676e36e2a630d91d19cff99608e
patricks1 <patrickstaudt1@gmail.com>
2024-11-11 18:20:37 -0800

    added nbformat to the setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="UCI_tools",
-    version="0.6.1",
+    version="0.6.2",
     author="UCI Bullock Galaxy Dark Matter Group",
     author_email="bullock@uci.edu",
     description="Software tools developed by and commonly used by the group.",
@@ -26,7 +26,8 @@ setuptools.setup(
         'pytest',
         'mock',
         'h5py',
-        'progressbar'
+        'progressbar',
+        'nbformat'
     ],
     entry_points={
         'console_scripts': [
